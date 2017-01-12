98bfcf1f768a897a83473189d380a58a77db13e5
christophn <christoph.niemann@maibornwolff.de>
2017-01-12 21:36:27 +0100

    Fix the bug that oraclize_bet was not calling its queryOracle but its parent's

--- contracts/OraclizeBet.sol
+++ contracts/OraclizeBet.sol
@@ -6,13 +6,25 @@ import './lib/StringLib.sol';
 
 contract OraclizeBet is Bet, usingOraclize {
 
-    event Info(string msg);
+    event Info(string message);
 
     function OraclizeBet() {
-        OAR = OraclizeAddrResolverI(0xbaedea311445c09489ba3e5cb8cee812a89099c4);
+        OAR = OraclizeAddrResolverI(0x87969a1f75cdd02744a32a9cc363f9acb1129873);
+    }
+
+    function evaluateBet() {
+
+        // determine winner is allowed in State.Closed only
+        if (state != State.Closed) {
+            throw;
+        }
+
+        queryOracle(pricelevel_string, '1477440000');
     }
 
     function queryOracle(string price, string startdate) {
+
+        Info('Called OraclizeBet.queryOracle()');
 
         string memory apiCall = strConcat('json(https://api.kraken.com/0/public/OHLC).result.XETHZUSD[?(@[2]>',
             price,
@@ -27,11 +39,11 @@ contract OraclizeBet is Bet, usingOraclize {
         Info(apiCall);
         Info(postData);
 
-        //oraclize_query('URL', apiCall, postData );
+        oraclize_query('URL', apiCall, postData );
     }
 
     // Callback to be called by once the oracle Query has been resolved
     function __callback(bytes32 myid, string result) public {
-        //Info(strConcat('Query result: ', result));
+        Info(strConcat('Query result: ', result));
     }
 }
